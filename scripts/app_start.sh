1b596d174353147d0c175f0087c7dc567ef0e275
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-11-08 02:31:17 -0700

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -4,4 +4,5 @@ export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 pm2 start index.js
 pm2 startup
+sudo env PATH=$PATH:/home/ec2-user/.nvm/versions/node/v16.16.0/bin pm2 startup systemd -u ec2-user --hp /home/ec2-user
 pm2 save
