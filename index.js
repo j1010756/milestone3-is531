a307b18cb443efef77d8fc776c0c9ac0a1755db3
j1010756 <106291794+j1010756@users.noreply.github.com>
2024-11-08 04:21:26 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 let express = require("express");
-let app = new express();                                                                     
+let app = new express();
+app.set("view engine", "ejs");
 
-// set up database connection
+// Set up database connection using environment variables
 const knex = require("knex")({
- client: "mysql",
- connection: {
-  host:"concert-db-instance-1.c61dq6ysma4i.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  password: "Password1",
-  database:"paradise-concerts",
-  port: 3306,
- },
+  client: "mysql",
+  connection: {
+    host: "milestone3-dbinstance-4lzf005ubyuz.cvm8gw8a626k.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "Milestone2!**",
+    database: "donut_db",
+    port: 3306,
+  },
 });
 
-app.get("/",(req,res) => {
- knex
- .select()
- .from("venues")
- .then((result) => {
-  console.log(result);
-  res.send(result);
- }); 
+app.get("/", (req, res) => {
+  knex
+    .select()
+    .from("donuts")
+    .then((result) => {
+      console.log(result);
+      res.render("index", { aDonutList: result });
+    });
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
